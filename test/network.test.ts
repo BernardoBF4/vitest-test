744a7ea665582c7009ba8a1a8071aac9efa693b9
BernardoBF4 <bernardo.fantin@gmail.com>
2023-11-05 20:05:21 -0300

    ✨ feat(requests: mocking): adicionado mock com msw

--- test/network.test.ts
+++ test/network.test.ts
@@ -1,15 +1,18 @@
-import { expect, test, vi } from 'vitest'
+import { afterAll, beforeAll, expect, test, vi } from 'vitest'
 import { getPost } from '../src/network'
+import { setupServer } from 'msw/node'
+import { HttpResponse, http } from 'msw'
 
-vi.stubGlobal('fetch', async () => {
-  return {
-    json() {
-      return { body: 'foo' }
-    },
-  }
-})
+const server = setupServer(
+  http.get('https://jsonplaceholder.typicode.com/posts/1', () => {
+    return HttpResponse.json({ body: 'Mocked!' })
+  })
+)
+
+beforeAll(() => server.listen())
+afterAll(() => server.close())
 
 test('should fetch', async () => {
   const result = await getPost(1)
-  expect(result).toMatchInlineSnapshot('"foo"')
+  expect(result).toMatchInlineSnapshot(`"Mocked!"`)
 })
